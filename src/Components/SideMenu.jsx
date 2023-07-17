862ba2b331c3caa726394de29928285789dcb2bb
Asma ABIDI <asma.abiidii@gmail.com>
2023-07-17 17:27:28 +0200

    Props Challenge

--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -11,7 +11,7 @@ function SideMenu() {
         </Button>
         <Button title="Data Science">
           <img
-            style={{width:"100px"}}
+            style={{width:"70px"}}
             src="https://images.unsplash.com/photo-1516520786482-9074007bee02?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
             alt=""
           />
@@ -21,9 +21,6 @@ function SideMenu() {
           <span>🐹🐹</span>
         </Button>
         <Button title="Self Improvement"></Button>
-        <Button />
-        <Button />
-        <Button />
       </div>
     </>
   );
